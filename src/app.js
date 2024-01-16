b654018385d200d8c7e3d6e92f536ed85c79b139
Jonathan Guillén <108660767+jguillen-at-wiris@users.noreply.github.com>
2024-01-16 12:33:17 +0100

    feat: add the handwriting mode only initialization

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const editableArea = document.getElementById('editable');
 const toolbarElement = document.getElementById('toolbar');
 const documentArea = document.getElementById('document');
 
-// Initialize MathType
-window.wrs_int_init(editableArea, toolbarElement);
+// Uncomment the next line to initialize MathType in classic mode
+// window.wrs_int_init(editableArea, toolbarElement);
 
-// Add a click event handler to the "Update" button
+// Uncomment the next line to initialize MathType in handwriting mode only
+//window.wrs_int_init(editableArea, toolbarElement, undefined, { forcedHandMode: true });
+
+// Add a click event handler to the "Update" button to render the editor content in an HTML element (document).
 document.getElementById('btn_update').addEventListener('click', (e) => {
     // Get and parse the editor contents
     const editorContent = WirisPlugin.Parser.endParse(editableArea.innerHTML);
